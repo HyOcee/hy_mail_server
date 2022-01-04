c5768ed6269634684d5758d9453da54440141e14
HyOcee <eosemegbe@gmail.com>
2022-01-04 14:32:33 +0100

    jfjf

--- server.js
+++ server.js
@@ -157,5 +157,5 @@ app.delete('/delete', (req,res) => {
 
 var PORT = process.env.PORT || 3000;
 app.listen((PORT), () => {
-    console.log(`app is running on port ${port}`);
+    console.log(`app is running on port ${PORT}`);
 })
